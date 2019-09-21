0f3d93204864384818a2e0171ee34b8d360bc566
abhi-386 <abhishekagarwal488@gmail.com>
2019-09-21 11:31:44 -0700

    Updates

---
+++ precache-manifest.6979430843a1df9385584c264541ac9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6800f4bddd7a757b358ad16b62800c6a",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "04f7b8561b8dde4a86ff",
+    "url": "/react-tutorial/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3b688acb99cd6a276942",
+    "url": "/react-tutorial/static/js/2.2827d546.chunk.js"
+  },
+  {
+    "revision": "04f7b8561b8dde4a86ff",
+    "url": "/react-tutorial/static/js/main.6015164c.chunk.js"
+  },
+  {
+    "revision": "8dd5ae209af12e0448ff",
+    "url": "/react-tutorial/static/js/runtime-main.bc1a34af.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-tutorial/static/media/logo.25bf045c.svg"
+  }
+]);
